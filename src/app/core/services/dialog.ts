18f710bc228de1ffdc55f90a051a77c9a2a8d5b6
akshayjka <akshayjai19001900@gmail.com>
2026-07-24 12:09:05 +0530

    Fix production logout alert  dialog

--- src/app/core/services/dialog.ts
+++ src/app/core/services/dialog.ts
@@ -22,11 +22,11 @@ export class DialogService {
 
       header,
 
-      subHeader: 'Secure School Van',
+      // subHeader: 'Secure School Van',
 
       message,
 
-      cssClass: 'universal-alert',
+      // cssClass: 'universal-alert',
 
       backdropDismiss: false,
 
@@ -55,42 +55,33 @@ export class DialogService {
 
 }
 
-  async confirmLogout(): Promise<boolean> {
-
+ async confirmLogout(): Promise<boolean> {
+  try {
     const alert = await this.alertController.create({
-
       header: 'Logout',
-
-      subHeader: 'Secure School Van',
-
-      message:
-        'Are you sure you want to logout from your account?',
-
-      cssClass: 'universal-alert',
-
+      message: 'Are you sure you want to logout from your account?',
       backdropDismiss: false,
-
       buttons: [
-
         {
           text: 'Cancel',
-
           role: 'cancel'
         },
-
         {
           text: 'Logout',
-
           role: 'confirm'
         }
-
       ]
     });
 
     await alert.present();
 
-    const result = await alert.onDidDismiss();
+    const { role } = await alert.onDidDismiss();
 
-    return result.role === 'confirm';
+    return role === 'confirm';
+
+  } catch (error) {
+    console.error('ALERT ERROR:', error);
+    return false;
   }
+}
 }
